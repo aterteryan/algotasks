aa945b8ba349bca1dacd0eba0f123cd059b77bb8
admin <admin@MacBook-Air-admin.local>
2018-03-20 15:46:01 +0300

    Add a second reverse_int solution

--- src/reverse_int/index.js
+++ src/reverse_int/index.js
@@ -15,11 +15,24 @@ function reverseInt(n) {
     .reverse()
     .join('');
 
-  if (n < 0) {
-    return parseInt(reversed) * -1;
-  }
-
-  return parseInt(reversed);
+  return parseInt(reversed) * Math.sign(n);
 }
 
 module.exports = reverseInt;
+
+
+// 1 solution
+//
+// function reverseInt(n) {
+//   const reversed = n
+//     .toString()
+//     .split('')
+//     .reverse()
+//     .join('');
+
+//   if (n < 0) {
+//     return parseInt(reversed) * -1;
+//   }
+
+//   return parseInt(reversed);
+// }
