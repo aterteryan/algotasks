c467a6562595623271fe6d7f22673bbbdeed20ad
admin <admin@MacBook-Air-admin.local>
2018-03-20 14:04:58 +0300

    add more solutions for reverse_string task

--- src/reverse_string/index.js
+++ src/reverse_string/index.js
@@ -7,10 +7,42 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-  return str
+  const reversed = [];
+
+  str
     .split('')
-    .reverse()
-    .join('');
+    .forEach((char) => reversed.unshift(char));
+
+  return reversed.join('');
 }
 
 module.exports = reverse;
+
+// 1 solution
+//
+// function reverse(str) {
+//   return str
+//     .split('')
+//     .reverse()
+//     .join('');
+// }
+
+// 2 solution
+//
+// function reverse(str) {
+//   let revStr = '';
+//
+//   for (let character of str) {
+//     revStr = character + revStr;
+//   }
+//
+//   return revStr;
+// }
+
+// 3 solution
+//
+// function reverse(str) {
+//   return str
+//     .split('')
+//     .reduce((rev, char) => rev = char + rev, '');
+// }
